c4ec3642dce92a553f8155a08e273daa1c9ace31
Jaydeep <83509207+jaydeepraj1012@users.noreply.github.com>
2024-12-14 17:18:48 +0530

    Create requirejs-config.js
    after define this code working on magento 2.4.7

---
+++ app/code/Vendor/Checkout/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+
+var config = {
+  map: {
+    '*': {
+      'Magento_Checkout/template/minicart/item/price.html':
+        'Vendor_Checkout/template/minicart/item/price.html',
+      // Letscms by jaydeep
+    }
+  }
+}
