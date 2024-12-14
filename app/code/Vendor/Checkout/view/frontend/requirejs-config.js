8e6106ada177d8b94d5ef3f9c8dbf3c0a2393294
Jaydeepletscms <jaydeep.letscms@gmail.com>
2024-12-14 16:39:19 +0530

    Create requirejs-config.js

---
+++ app/code/Vendor/Checkout/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+  map: {
+    '*': {
+      'Magento_Checkout/template/minicart/item/price.html':
+        'Vendor_Checkout/template/minicart/item/price.html',
+      // Letscms by jaydeep
+    }
+  }
+}
